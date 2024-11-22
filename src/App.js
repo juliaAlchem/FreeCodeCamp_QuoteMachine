68884ad8fc31fdd8236adc2c2ef6340e609c2f24
juliaAlchem <julialchem@gmail.com>
2024-11-22 20:44:30 +0100

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {useEffect, useState} from 'react';
 import './App.scss';
 import { cleanup } from '@testing-library/react';
+import COLORS_ARRAY from './colorsArray.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+
 
 let quoteDBUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
@@ -9,6 +13,7 @@ function App() {
   const [author, setAuthor] = useState('Norman Vaughan')
   const [randomNumber, setRandomNumber] = useState(0)
   const [quotesArray, setQuotesArray] = useState(null)
+  const [accentColor, setAccentColor] = useState('#F4511E')
 
  ////////////////// Check this part
   const fetchQuotes = async (url) => {
@@ -26,19 +31,27 @@ function App() {
   const getRandomQuote = () => {
     let randomNum = Math.floor(quotesArray.length * Math.random())
     setRandomNumber(randomNum);
+    setAccentColor(COLORS_ARRAY[randomNum])
     setQuote(quotesArray[randomNum].quote)
     setAuthor(quotesArray[randomNum].author)
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <section id='quote-box'>
-        <h1>Random Number: {randomNumber}</h1>
+      <header className="App-header" style={
+        {backgroundColor: accentColor}}>
+        <section id='quote-box' style={
+        {color: accentColor}}>
         <p id='text'>"{quote}"</p>
         <p id='author'>- {author}</p>
-       <button id="new-quote" onClick={() => {getRandomQuote()}}>New Quote</button>
-       <a id='tweet-quote' href={`twitter.com/intent/tweet`} target="_blank">Tweet</a>
+
+        <div class='buttons'> 
+       <button id="new-quote" onClick={() => {getRandomQuote()}} style={
+        {backgroundColor: accentColor}}>New Quote</button>
+       <a id='tweet-quote' href={`twitter.com/intent/tweet`} target="_blank" style={
+        {backgroundColor: accentColor}}><FontAwesomeIcon icon={faTwitter} /></a>
+        </div>
+
        </section>
       </header>
     </div>
